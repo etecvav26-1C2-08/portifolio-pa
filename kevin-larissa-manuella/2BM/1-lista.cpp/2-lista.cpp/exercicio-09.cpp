523ba71693012c831e1072ff2b17898bd0dddd71
Kevin Will <kevinwilliandasilvasouza@gmail.com>
2026-06-15 14:42:36 -0300

    Create exercicio-09.cpp

---
+++ kevin-larissa-manuella/2BM/1-lista.cpp/2-lista.cpp/exercicio-09.cpp
@@ -0,0 +1,34 @@
+/* EtecVAV 14/06/2026
+Descrição: Ler uma palavra. Substituir todas as vogais por */
+#include <iostream>
+#include <string>
+using namespace std;
+
+string substituicaoVogais(string palavra) {
+    
+    for(int i = 0; i < palavra.length(); i++) {
+        
+         if(palavra[i] == 'a' || palavra[i] == 'e' ||
+           palavra[i] == 'i' || palavra[i] == 'o' ||
+           palavra[i] == 'u') {
+               
+            palavra[i] = '*';
+         }
+    }
+    
+    return palavra;
+}
+    
+int main() {
+    
+    string palavra;
+    
+    cout << "Escreva uma palavra:\n";
+    cin >> palavra;
+    
+    palavra = substituicaoVogais(palavra);
+    
+    cout << palavra;
+
+    return 0;
+}
