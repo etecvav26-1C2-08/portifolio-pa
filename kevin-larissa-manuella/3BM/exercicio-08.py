9bd7d6b56efca3f3d5f50766969d0518a348fa26
guk <manuu.salvador2011@gmail.com>
2026-08-13 13:44:11 -0300

    Update exercicio-08.py

---
+++ kevin-larissa-manuella/3BM/exercicio-08.py
@@ -0,0 +1,24 @@
+    # Autor: Manuella Salvador
+    # Data: Junho/2026
+    # Descrição: Verifica se um número inteiro positivo é primo.
+
+
+numero = int(input("Digite um número inteiro positivo: "))
+
+if numero > 0:
+    divisor = 1
+    quantidade_divisores = 0
+
+    while divisor <= numero:
+    
+        if numero % divisor == 0:
+            quantidade_divisores += 1
+
+        divisor += 1
+
+    if quantidade_divisores == 2:
+        print("O número é primo.")
+    else:
+        print("O número não é primo.")
+else:
+    print("Número invalido.")
