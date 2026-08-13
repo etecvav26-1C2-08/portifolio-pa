7317fd5770fb43dd857a127b924bcb086aa9aae8
guk <manuu.salvador2011@gmail.com>
2026-08-13 13:44:53 -0300

    Create exercicio-09.py

---
+++ kevin-larissa-manuella/3BM/exercicio-09.py
@@ -0,0 +1,18 @@
+    # Autor: Manuella Salvador
+    # Data: Junho/2026
+    # Descrição: Calcula o fatorial de um número inteiro positivo.
+
+
+numero = int(input("Digite um número inteiro positivo: "))
+
+if numero >= 0:
+    contador = 1
+    fatorial = 1
+
+    while contador <= numero:
+        fatorial *= contador  
+        contador += 1
+
+    print(f"Fatorial: {fatorial}")
+else:
+    print("Número invalido.")
